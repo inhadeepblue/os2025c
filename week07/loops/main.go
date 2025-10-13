39f1842d4b3d99ca7c94587535ac393de5012169
inhadeepblue <deepblue@inha.ac.kr>
2025-10-13 17:17:43 +0900

    loops/main.go v0.1 bufio & os packages

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
-	"time"
+	"os"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var month time.Month = now.Month() // month := now.Month()
-	var day int = now.Day()
-	fmt.Println(month, day)
-
-	// var univ string = "Go$ Inha$"
-	changer := strings.NewReplacer("$", "!")
-	// changed := changer.Replace(univ)
-	changed := changer.Replace("Go$ Inha$")
-	fmt.Println(changed)
+	r := bufio.NewReader(os.Stdin)
+	i, _ := r.ReadString('\n') // ignore error
+	fmt.Println(i)
 }
