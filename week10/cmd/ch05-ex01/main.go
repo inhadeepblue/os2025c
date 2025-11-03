477b88ad8572e741bc46e6ca33aee160acacc7bb
inhadeepblue <deepblue@inha.ac.kr>
2025-11-03 18:23:13 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
-	arrayBool := [2]bool{true, false}
-	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < len(arrayInt); i++ {
-		fmt.Println(i, arrayBool[i])
-		fmt.Println(i, arrayInt[i])
+	// numbers := [3]int{-9, 11, 7}
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(i, numbers[i])
+	// }
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers {
+		fmt.Println(i, number)
 	}
 }
